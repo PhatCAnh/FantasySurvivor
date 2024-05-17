8a7c36d98b4ce412ca333c4ec9212a435a903f76
Longgg <nhatlongnguyen2003@gmail.com>
2024-05-17 19:59:40 +0700

    update prefab BoneWeaver

--- FantasySurvivor/Assets/_App/Scripts/Views/InGame/Monsters/State_Monster/MonsterSummoner.cs
+++ FantasySurvivor/Assets/_App/Scripts/Views/InGame/Monsters/State_Monster/MonsterSummoner.cs
@@ -1,18 +1,31 @@
+using _App.Scripts.Controllers;
+using ArbanFramework;
+using FantasySurvivor;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MonsterSummoner : Monster
 {
-
+   
     private GameController gameController => ArbanFramework.Singleton<GameController>.instance;
+
+    private int numMonster = 3;
+    private float range = 2f;
+
+
 
     public override void Attack()
     {
-        var mob = gameController.SpawnMonster("M1", model.attackDamage, Mathf.RoundToInt(model.maxHealthPoint), 0);
-        mob.transform.position = transform.position;
-    
-    
-    }
+        for (int i = 0; i < numMonster; i++)
+        {
+            var mob = gameController.SpawnMonster("Skeleton", 0, 10,3);
 
+            Vector2 randomPosition = new Vector2(firePoint.position.x + Random.Range(-range, range),
+                                                 firePoint.position.y + Random.Range(-range, range));
+
+            mob.transform.position = randomPosition;
+        }
+    }
+ 
 }
