5968ef8d047fbce468158f349bb5bce8da1b1b64
Longgg <nhatlongnguyen2003@gmail.com>
2024-06-06 13:51:49 +0700

    update bullet for GatlingCrab

---
+++ FantasySurvivor/Assets/_App/Scripts/Views/InGame/Monsters/State_Boss/InteractBulletBoss.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using FantasySurvivor;
+using UnityEngine;
+namespace FantasySurvivor
+{
+	
+	public class InteractBulletBoss : BulletBossGatlingCrab
+	{
+		protected override void OnViewInit()
+		{
+			base.OnViewInit();
+			if(!app.models.dataPlayerModel.firstSeeBulletInteract)
+			{
+				app.resourceManager.ShowPopup(PopupType.ClickBulletTutorial);
+				app.models.dataPlayerModel.firstSeeBulletInteract = true;
+			}
+		}
+
+		private void OnMouseDown()
+		{
+			if(gameController.isStop) return;
+			
+			Touch();
+		}
+	}
+}
