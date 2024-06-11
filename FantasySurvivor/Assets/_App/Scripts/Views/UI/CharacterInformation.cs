d4da6decac33091db969b8952b57681875ba6b99
Thanh <Thanhndce160270@fpt.edu.vn>
2024-06-11 23:52:35 +0700

    update show Item character infor UI and logic
    update show Item character infor UI and logic

--- FantasySurvivor/Assets/_App/Scripts/Views/UI/CharacterInformation.cs
+++ FantasySurvivor/Assets/_App/Scripts/Views/UI/CharacterInformation.cs
@@ -1,172 +1,3 @@
-//using System;
-//using ArbanFramework.MVC;
-//using FantasySurvivor;
-//using System.Collections;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Threading.Tasks;
-//using TMPro;
-//using Unity.VisualScripting;
-//using UnityEngine;
-//using UnityEngine.Serialization;
-//using UnityEngine.UI;
-
-//public class CharacterInformation : View<GameApp>, IPopup
-//{
-//	[SerializeField] private TextMeshProUGUI _txtHealth, _txtMoveSpeed, _txtAtkDamage, _txtArmor;
-
-//	[SerializeField] private Transform _slotItemEquipContainer;
-
-//	[SerializeField] private GameObject _slotItemEquipPrefab;
-
-//	[SerializeField] private ItemSlotEquipUI _slotWeapon, _slotArmor, _slotHat, _slotRing, _slotShoes, _slotGloves;
-
-//	[SerializeField] private Button _btnBack;
-
-//	private Dictionary<ItemType, ItemSlotEquipUI> _dicItemEquip;
-
-//	private ItemController itemController => ArbanFramework.Singleton<ItemController>.instance;
-
-//	private List<GameObject> _listItemSlot = new List<GameObject>();
-
-//	protected override void Start()
-//	{
-//		base.Start();
-
-//		//fix cho nay
-//		Task.Delay(500);
-
-//		InitDispatcher();
-//	}
-
-//	protected override void OnViewInit()
-//	{
-//		base.OnViewInit();
-
-//		_slotWeapon.Init(null, this);
-//		_slotArmor.Init(null, this);
-//		_slotHat.Init(null, this);
-//		_slotRing.Init(null, this);
-//		_slotShoes.Init(null, this);
-//		_slotGloves.Init(null, this);
-
-//		_dicItemEquip = new Dictionary<ItemType, ItemSlotEquipUI>
-//		{
-//			{ItemType.Weapon, _slotWeapon},
-//			{ItemType.Armor, _slotArmor},
-//			{ItemType.Hat, _slotHat},
-//			{ItemType.Ring, _slotRing},
-//			{ItemType.Shoes, _slotShoes},
-//			{ItemType.Gloves, _slotGloves},
-//		};
-
-//		_btnBack.onClick.AddListener(Close);
-
-//		//app.models.dataPlayerModel.AddItemEquipToBag(ItemId.Axe, ItemRank.Legendary, 2);
-
-//		foreach (var item in app.models.dataPlayerModel.BagItem)
-//		{
-//			var go = Instantiate(_slotItemEquipPrefab, _slotItemEquipContainer);
-//			go.TryGetComponent(out ItemSlotUI item1);
-//			item1.Init(item, this);
-//			_listItemSlot.Add(go);
-//		}
-
-//		foreach (var item in app.models.dataPlayerModel.ListItemEquipped)
-//		{
-//			var data = itemController.GetDataItem(item.id, item.rank, item.level);
-//			var slot = _dicItemEquip[data.dataConfig.type];
-//			if (slot.isEquip) UnEquipItem(data.dataConfig.type, item);
-//			slot.Init(item);
-//		}
-//	}
-
-//	[ContextMenu("Test item piece")]
-//	public void Test()
-//	{
-//		app.models.dataPlayerModel.AddItemEquipToBag(ItemId.PieceFire, 2);
-//	}
-
-//	[ContextMenu("Test item Equip")]
-//	public void TestEquip()
-//	{
-//		app.models.dataPlayerModel.AddItemEquipToBag(ItemId.Axe, ItemRank.Legendary, 5);
-//	}
-
-//	public void EquipItem(ItemType type, ItemInBag data)
-//	{
-//		var slot = _dicItemEquip[type];
-//		if (slot.isEquip) UnEquipItem(type, data);
-//		slot.Init(data);
-//		itemController.EquipItem(data);
-//	}
-
-//	public void UnEquipItem(ItemType type, ItemInBag data)
-//	{
-//		_dicItemEquip[type].ResetData();
-//		itemController.UnEquipItem(data);
-//		Instantiate(_slotItemEquipPrefab, _slotItemEquipContainer).TryGetComponent(out ItemSlotUI item);
-//		item.Init(data, this);
-//	}
-
-//	private void InitDispatcher()
-//	{
-//		AddDataBinding("fieldCharacterModel-healthValue", _txtHealth, (control, e) =>
-//			{
-//				control.text = $"{app.models.characterModel.maxHealthPoint}";
-//			}, new DataChangedValue(CharacterModel.dataChangedEvent, nameof(CharacterModel.maxHealthPoint), app.models.characterModel)
-//		);
-
-//		AddDataBinding("fieldCharacterModel-moveSpeedValue", _txtMoveSpeed, (control, e) =>
-//			{
-//				control.text = $"{app.models.characterModel.moveSpeed}";
-//			}, new DataChangedValue(CharacterModel.dataChangedEvent, nameof(CharacterModel.moveSpeed), app.models.characterModel)
-//		);
-
-//		AddDataBinding("fieldCharacterModel-atkDamageValue", _txtAtkDamage, (control, e) =>
-//			{
-//				control.text = $"{app.models.characterModel.attackDamage}";
-//			}, new DataChangedValue(CharacterModel.dataChangedEvent, nameof(CharacterModel.attackDamage), app.models.characterModel)
-//		);
-
-//		AddDataBinding("fieldCharacterModel-armorValue", _txtArmor, (control, e) =>
-//		{
-//			float armorPercent = app.models.characterModel.armor / 100f;
-//			control.text = $"{armorPercent * 100f}%";
-//		}, new DataChangedValue(CharacterModel.dataChangedEvent, nameof(CharacterModel.armor), app.models.characterModel));
-
-//		AddDataBinding("fieldDataPlayerModel-BagItemEquipValue", this, (control, e) =>
-//			{
-//				foreach (var itemSlot in _listItemSlot.ToList())
-//				{
-//					Destroy(itemSlot);
-//				}
-//				_listItemSlot.Clear();
-
-//				foreach (var itemSlot in app.models.dataPlayerModel.BagItem)
-//				{
-//					var go = Instantiate(_slotItemEquipPrefab, _slotItemEquipContainer);
-//					go.TryGetComponent(out ItemSlotUI item1);
-//					item1.Init(itemSlot, this);
-//					_listItemSlot.Add(go);
-//				}
-
-//				//             var dataStat = app.models.dataPlayerModel.GetFirstItemEquipAdded();
-//				// if (dataStat == null) return;
-//				//             Instantiate(_slotItemEquipPrefab, _slotItemEquipContainer).TryGetComponent(out ItemSlotUI item);
-//				//             item.Init(dataStat, this);
-//			}, new DataChangedValue(DataPlayerModel.dataChangedEvent, nameof(DataPlayerModel.BagItem), app.models.dataPlayerModel)
-//		);
-//	}
-//	public void Open()
-//	{
-
-//	}
-//	public void Close()
-//	{
-//		Destroy(gameObject);
-//	}
-//}
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -184,6 +15,20 @@ public class CharacterInformation : View<GameApp>, IPopup
     [SerializeField] private GameObject _slotItemEquipPrefab;
     [SerializeField] private ItemSlotEquipUI _slotWeapon, _slotArmor, _slotHat, _slotRing, _slotShoes, _slotGloves;
     [SerializeField] private Button _btnBack;
+    [SerializeField] private Button _btnAll;
+    [SerializeField] private GameObject _focusAll;
+    [SerializeField] private Button _btnWeapon;
+    [SerializeField] private GameObject _focusWeapon;
+    [SerializeField] private Button _btnArmor;
+    [SerializeField] private GameObject _focusArmor;
+    [SerializeField] private Button _btnShoes;
+    [SerializeField] private GameObject _focusShoes;
+    [SerializeField] private Button _btnRing;
+    [SerializeField] private GameObject _focusRing;
+    [SerializeField] private Button _btnHat;
+    [SerializeField] private GameObject _focusHat;
+    [SerializeField] private Button _btnGlove;
+    [SerializeField] private GameObject _focusGlove;
 
     private Dictionary<ItemType, ItemSlotEquipUI> _dicItemEquip;
     private ItemController itemController => ArbanFramework.Singleton<ItemController>.instance;
@@ -218,8 +63,84 @@ public class CharacterInformation : View<GameApp>, IPopup
         };
 
         _btnBack.onClick.AddListener(Close);
+        _btnAll.onClick.AddListener(()=>
+        {
+            ShowAll();
+            ToggleFocus(_focusAll);
 
-        // Sort the bag items and equipped items based on the desired order
+        });
+        _btnWeapon.onClick.AddListener(() =>
+        {
+            ShowItemsByType(ItemType.Weapon);
+            ToggleFocus(_focusWeapon);
+            _focusAll.SetActive(false);
+          
+        });
+        _btnArmor.onClick.AddListener(() =>
+        {
+            ShowItemsByType(ItemType.Armor);
+            ToggleFocus(_focusArmor);
+            _focusAll.SetActive(false);
+        });
+        _btnShoes.onClick.AddListener(() =>
+        {
+            ShowItemsByType(ItemType.Shoes);
+            ToggleFocus(_focusShoes);
+            _focusAll.SetActive(false);
+        });
+        _btnRing.onClick.AddListener(() =>
+        {
+            ShowItemsByType(ItemType.Ring);
+            ToggleFocus(_focusRing);
+           _focusAll.SetActive(false);
+        });
+        _btnHat.onClick.AddListener(() =>
+        {
+            ShowItemsByType(ItemType.Hat);
+            ToggleFocus(_focusHat);
+            _focusAll.SetActive(false);
+        });
+       _btnGlove.onClick.AddListener(() =>
+        {
+            ShowItemsByType(ItemType.Gloves);
+            ToggleFocus(_focusGlove);
+            _focusAll.SetActive(false);
+        });
+
+        ShowAll();
+    }
+
+    private void ShowItemsByType(ItemType type)
+    {
+        foreach (var itemSlot in _listItemSlot)
+        {
+            Destroy(itemSlot);
+        }
+        _listItemSlot.Clear();
+
+        var filteredItems = app.models.dataPlayerModel.BagItem
+            .Where(item => itemController.GetDataItem(item.id, item.rank, item.level).dataConfig.type == type)
+            .ToList();
+
+        foreach (var item in filteredItems)
+        {
+            var go = Instantiate(_slotItemEquipPrefab, _slotItemEquipContainer);
+            if (go.TryGetComponent(out ItemSlotUI item1))
+            {
+                item1.Init(item, this);
+                _listItemSlot.Add(go);
+            }
+        }
+    }
+
+    private void ShowAll()
+    {
+        foreach (var itemSlot in _listItemSlot)
+        {
+            Destroy(itemSlot);
+        }
+        _listItemSlot.Clear();
+
         var sortedBagItems = app.models.dataPlayerModel.BagItem
             .OrderBy(item => GetItemTypeOrder(itemController.GetDataItem(item.id, item.rank, item.level).dataConfig.type))
             .ToList();
@@ -246,6 +167,17 @@ public class CharacterInformation : View<GameApp>, IPopup
             slot.Init(item);
         }
     }
+    private void ToggleFocus(GameObject focusObject)
+    {
+       _focusAll.SetActive(focusObject == _focusAll);
+        _focusWeapon.SetActive(focusObject == _focusWeapon);
+        _focusArmor.SetActive(focusObject == _focusArmor);
+        _focusShoes.SetActive(focusObject == _focusShoes);
+        _focusRing.SetActive(focusObject == _focusRing);
+        _focusHat.SetActive(focusObject == _focusHat);
+        _focusGlove.SetActive(focusObject == _focusGlove);
+    }
+
 
     private int GetItemTypeOrder(ItemType itemType)
     {
@@ -261,8 +193,6 @@ public class CharacterInformation : View<GameApp>, IPopup
             default: return int.MaxValue;
         }
     }
-
-   
 
     public void EquipItem(ItemType type, ItemInBag data)
     {
@@ -283,6 +213,7 @@ public class CharacterInformation : View<GameApp>, IPopup
             _listItemSlot.Add(go);
         }
     }
+
     [ContextMenu("Test item piece")]
     public void Test()
     {
@@ -313,9 +244,6 @@ public class CharacterInformation : View<GameApp>, IPopup
         {
             control.text = $"{app.models.characterModel.maxHealthPoint}";
         }, new DataChangedValue(CharacterModel.dataChangedEvent, nameof(CharacterModel.maxHealthPoint), app.models.characterModel));
-    
-	
-
 
         AddDataBinding("fieldCharacterModel-moveSpeedValue", _txtMoveSpeed, (control, e) =>
         {
