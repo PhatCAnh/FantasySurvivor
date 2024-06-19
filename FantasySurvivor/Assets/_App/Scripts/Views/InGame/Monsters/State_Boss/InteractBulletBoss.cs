81c5a7078e4c19d67438cd0b53760f781eeb766f
PhatCAnh <136288166+PhatCanAnh@users.noreply.github.com>
2024-06-19 15:48:33 +0700

    Fix bug

--- FantasySurvivor/Assets/_App/Scripts/Views/InGame/Monsters/State_Boss/InteractBulletBoss.cs
+++ FantasySurvivor/Assets/_App/Scripts/Views/InGame/Monsters/State_Boss/InteractBulletBoss.cs
@@ -6,16 +6,6 @@ namespace FantasySurvivor
 	
 	public class InteractBulletBoss : BulletBossGatlingCrab
 	{
-		protected override void OnViewInit()
-		{
-			base.OnViewInit();
-			if(!app.models.dataPlayerModel.firstSeeBulletInteract)
-			{
-				app.resourceManager.ShowPopup(PopupType.ClickBulletTutorial);
-				app.models.dataPlayerModel.firstSeeBulletInteract = true;
-			}
-		}
-
 		private void OnMouseDown()
 		{
 			if(gameController.isStop) return;
