97824e315df34388ceb203486b755793a39a0efa
Longgg <nhatlongnguyen2003@gmail.com>
2024-07-12 16:34:08 +0700

    create logic for new minion Crabbomber

--- FantasySurvivor/Assets/_App/Scripts/Views/InGame/Monsters/State_SumMonster/MinionMonster.cs
+++ FantasySurvivor/Assets/_App/Scripts/Views/InGame/Monsters/State_SumMonster/MinionMonster.cs
@@ -9,8 +9,11 @@ public class MinionMonster : Monster
     private Collider2D monsCollider;
     private GameController gameController => Singleton<GameController>.instance;
 
+
+
     public override void TakeDamage(float damage, TextPopupType type, bool isCritical = false, Action callBackDamaged = null, Action callBackKilled = null)
     {
+        base.TakeDamage(damage, type, isCritical, callBackDamaged, callBackKilled);
         this.Die();
     }
 
