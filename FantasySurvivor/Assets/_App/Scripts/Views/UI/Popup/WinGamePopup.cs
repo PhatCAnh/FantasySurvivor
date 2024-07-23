dc28a0be03487646684b2eb73414a1809aa80e0a
Longgg <nhatlongnguyen2003@gmail.com>
2024-07-23 18:41:02 +0700

    Create logic win game

---
+++ FantasySurvivor/Assets/_App/Scripts/Views/UI/Popup/WinGamePopup.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using ArbanFramework;
+using ArbanFramework.MVC;
+using DG.Tweening;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinGame : View<GameApp>, IPopup
+{
+    [SerializeField] private Image _imageBackGround;
+    [SerializeField] private GameObject _title;
+    [SerializeField] private TextMeshProUGUI _txtEnemyKilled, _txtWave;
+
+    [SerializeField] private GameObject _leftArrow;
+    [SerializeField] private GameObject _rightArrow;
+    [SerializeField] private TextMeshProUGUI _txtReward;
+    [SerializeField] private Transform _containerReward;
+
+    [SerializeField] private Button _btnClaim;
+    [SerializeField] private Button _btnReward;
+
+    private Sequence _sequence;
+    private GameController gameController => Singleton<GameController>.instance;
+
+    protected override void OnViewInit()
+    {
+        base.OnViewInit();
+
+        foreach (var reward in gameController.map.dictionaryReward)
+        {
+            gameController.SpawnItemReward(reward.Key, reward.Value, _containerReward);
+        }
+
+        _btnClaim.onClick.AddListener(OnClickBtnClaim);
+        _btnReward.onClick.AddListener(OnClickBtnReward);
+
+        _txtWave.text = $"Best wave: {gameController.map.model.WaveInGame}";
+
+        _txtEnemyKilled.text = $"Monster killed: {gameController.map.model.monsterKilled}";
+
+        Open();
+    }
+
+    public void Open()
+    {
+        SetAnimOpen();
+    }
+
+    public void Close()
+    {
+        gameController.ResetGame();
+        gameController.ChangeSceneHome();
+        gameController.isEndGame = false;
+        Destroy(gameObject);
+    }
+
+    private void OnClickBtnClaim()
+    {
+        foreach (var reward in gameController.map.dictionaryReward)
+        {
+            gameController.ClaimReward(reward.Key, reward.Value);
+        }
+
+        Close();
+    }
+
+    private void OnClickBtnReward()
+    {
+        app.adsController.ShowReward(() =>
+        {
+            foreach (var reward in gameController.map.dictionaryReward)
+            {
+                gameController.ClaimReward(reward.Key, reward.Value * 2);
+            }
+
+            Close();
+        });
+    }
+
+    private void SetAnimOpen()
+    {
+        _sequence = DOTween.Sequence();
+        var duration = 0.25f;
+        _imageBackGround.color = Color.clear;
+        _title.transform.localScale = Vector3.zero;
+        _txtEnemyKilled.transform.localScale = Vector3.zero;
+        _txtWave.transform.localScale = Vector3.zero;
+        _txtReward.transform.localScale = Vector3.zero;
+        _leftArrow.transform.localPosition = new Vector3(-1000, 0);
+        _rightArrow.transform.localPosition = new Vector3(1000, 0);
+
+        for (int i = 0; i < _containerReward.childCount; i++)
+        {
+            _containerReward.GetChild(i).localScale = Vector3.zero;
+        }
+
+        _btnClaim.transform.localScale = Vector3.zero;
+        _btnReward.transform.localScale = Vector3.zero;
+
+
+        _sequence
+            .Append(_imageBackGround.DOFade(0.9f, duration * 2))
+            .Join(_title.transform.DOScale(Vector3.one, duration * 2))
+            .Append(_txtEnemyKilled.transform.DOScale(Vector3.one, duration))
+            .Append(_txtWave.transform.DOScale(Vector3.one, duration))
+            .Append(_txtReward.transform.DOScale(Vector3.one, duration))
+            .Join(_leftArrow.transform.DOLocalMove(new Vector3(-470, 0), duration))
+            .Join(_rightArrow.transform.DOLocalMove(new Vector3(470, 0), duration))
+            .Join(_btnClaim.transform.DOScale(Vector3.one, duration))
+            .Join(_btnReward.transform.DOScale(Vector3.one, duration));
+
+
+        for (int i = 0; i < _containerReward.childCount; i++)
+        {
+            _sequence.Append(_containerReward.GetChild(i).DOScale(Vector3.one, duration / 2));
+        }
+    }
+
+
+    protected override void OnDestroy()
+    {
+        base.OnDestroy();
+        _sequence.Kill();
+    }
+}
